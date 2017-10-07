c23ec3f5cd9562ce3442e37d8f6bfa010b7cf643
Ertugrul Cetin <ertugrulcetin@Ertugruls-MBP.fritz.box>
2017-10-07 17:03:47 +0200

    tiny maze added

--- src/code_katas/katas/wonderland.clj
+++ src/code_katas/katas/wonderland.clj
@@ -159,7 +159,47 @@
                                                         (is (= :player1 (play-game [[:heart 10]] [[:heart 9]])) :default :advanced)
                                                         (is (= :player2 (play-game [[:heart 9]] [[:heart 10]])) :default :advanced)))
 
-                                     ))))
+                                     ))
+
+               (subject 'sbj-tiny-maze
+                        "Tiny Maze"
+
+                        (learn
+                          (text
+                            (p "Alice found herself very tiny and wandering around Wonderland. Even the grass around her seemed like a maze.")
+                            (p "This is a tiny maze solver.")
+                            (p "A maze is represented by a matrix:")
+                            (code "[[:S 0 1]\n [1  0 1]\n [1  0 :E]]")
+                            (p "S : start of the maze")
+                            (p "E : end of the maze")
+                            (p "1 : This is a wall that you cannot pass through")
+                            (p "0 : A free space that you can move through")
+                            (p "The goal is the get to the end of the maze. A solved maze will have a :x in the start, the path, and the end of the maze, like this:")
+                            (code "[[:x :x 1]\n [1  :x 1]\n [1  :x :x]]")))
+
+                        (instruction 'ins-tiny-maze
+                                     (run-pre-tests? true)
+                                     (initial-code "(ns tiny-maze.solver)\n\n\n(defn solve-maze \n  [maze]\n  ;fill me\n  )")
+                                     (rule :no-rule? true)
+
+                                     (sub-instruction 'sub-ins-3x3-maze
+                                                      (text
+                                                        (p "Can find way to exit with 3x3 maze:")
+                                                        (code "(let [maze [[:S 0 1]\n            [1 0 1]\n            [1 0 :E]]\n      sol  [[:x :x 1]\n            [1 :x 1]\n            [1 :x :x]]]\n  (= sol (solve-maze maze)))"))
+
+                                                      (testing
+                                                        (is (= [[:x :x 1] [1 :x 1] [1 :x :x]]
+                                                               (solve-maze [[:S 0 1] [1 0 1] [1 0 :E]])) :default :advanced)))
+
+                                     (sub-instruction 'sub-ins-4x4-maze
+                                                      (text
+                                                        (p "Can find way to exit with 4x4 maze:")
+                                                        (code "(let [maze [[:S 0 0 1]\n            [1 1 0 0]\n            [1 0 0 1]\n            [1 1 0 :E]]\n      sol  [[:x :x :x 1]\n            [1 1 :x 0]\n            [1 0 :x 1]\n            [1 1 :x :x]]]\n  (= sol (solve-maze maze)))"))
+
+                                                      (testing
+                                                        (is (= [[:x :x :x 1] [1 1 :x 0] [1 0 :x 1] [1 1 :x :x]]
+                                                               (solve-maze [[:S 0 0 1] [1 1 0 0] [1 0 0 1] [1 1 0 :E]])) :default :advanced)))))))
+
 
 (defcoursetest my-wonderful-test-1
                [ch-katas sub-ch-wonderland sbj-alphabet-cipher ins-alphabet-cipher sub-ins-encode]
@@ -432,3 +472,169 @@
                          :else :draw))))
 
 
+(defcoursetest my-wonderful-test-12
+               [ch-katas sub-ch-wonderland sbj-tiny-maze ins-tiny-maze sub-ins-3x3-maze]
+               (defn- row-count
+                 "Returns the row count of the maze.
+                  (row-count [[1] [2] [3]])
+                  => 3"
+                 [maze]
+                 (count maze))
+
+               (defn- col-count
+                 "Returns the column count of the maze.
+                  (col-count [[1 2 3]])
+                  => 3"
+                 [maze]
+                 (count (first maze)))
+
+               (defn- start-position
+                 "Finds the start position in the maze.
+                  (start-position [[:S 0 1] [0 0 1] [1 0 :E]])
+                  => [0 0]"
+                 [maze]
+                 (let [index (.indexOf (flatten maze) :S)]
+                   [(unchecked-divide-int index (row-count maze)) (mod index (col-count maze))]))
+
+               (defn- in-bounds?
+                 "Checks to see if the passed position is in the bounds of the row and columns of the maze.
+                  (in-bounds? 3 3 [4 0])
+                  => false
+                  (in-bounds? 3 3 [1 1])
+                  => true"
+                 [rows cols [x y]]
+                 (not (or (< x 0) (< y 0) (>= x rows) (>= y cols))))
+
+               (defn- valid-move?
+                 "Checks to see if the position is a valid position given a move. Valid positions are only those that are in bounds and
+                  have either a 0 or :E in their spot.
+                  (valid-move? [[:x :x 1] [0 0 1] [1 0 :E]] [1 2])
+                  => false
+                  (valid-move? [[:x :x 1] [0 0 1] [1 0 :E]] [1 1])
+                  => true"
+                 [move pos]
+                 (let [value (get-in move pos)]
+                   (or (= value 0) (= value :E))))
+
+               (defn- next-moves
+                 "Returns only the next valid positions given a maze and a current position.
+                 (next-moves [0 0] [[:x 0 1] [0 0 1] [1 0 :E]])
+                 => ([1 0] [0 1])"
+                 [[x y] maze]
+                 (let [rows      (row-count maze)
+                       cols      (col-count maze)
+                       positions [[(+ x 1) y] [x (+ y 1)] [x (- y 1)] [(- x 1) y]]]
+                   (filter #(and (in-bounds? rows cols %) (valid-move? maze %)) positions)))
+
+               (defn- solved?
+                 "Checks to see if the maze is solved by determining if there is an :E in the maze.
+                  (solved? [[:x :x 1] [1 :x 1] [1 :x :x]])
+                  => true
+                  (solved? [[:x :x 1] [1 :x 1] [1 :x :E]])
+                  => false"
+                 [maze]
+                 (= -1 (.indexOf (flatten maze) :E)))
+
+               (defn- solutions
+                 "Finds all possible solutions to the maze."
+                 [position moves]
+                 (let [current-move (last moves)
+                       next-moves   (next-moves position current-move)]
+                   (if-not (seq next-moves)
+                     (when (solved? current-move)
+                       [moves])
+                     (mapcat #(solutions % (conj moves (assoc-in current-move % :x))) next-moves))))
+
+               (defn solve-maze
+                 "Solves the maze and returns the shortest solution."
+                 [maze]
+                 (let [start-position (start-position maze)
+                       initial-moves  [(assoc-in maze start-position :x)]]
+                   (->> (solutions start-position initial-moves)
+                        (sort-by count)
+                        first
+                        last))))
+
+
+(defcoursetest my-wonderful-test-13
+               [ch-katas sub-ch-wonderland sbj-tiny-maze ins-tiny-maze sub-ins-4x4-maze]
+               (defn- row-count
+                 "Returns the row count of the maze.
+                  (row-count [[1] [2] [3]])
+                  => 3"
+                 [maze]
+                 (count maze))
+
+               (defn- col-count
+                 "Returns the column count of the maze.
+                  (col-count [[1 2 3]])
+                  => 3"
+                 [maze]
+                 (count (first maze)))
+
+               (defn- start-position
+                 "Finds the start position in the maze.
+                  (start-position [[:S 0 1] [0 0 1] [1 0 :E]])
+                  => [0 0]"
+                 [maze]
+                 (let [index (.indexOf (flatten maze) :S)]
+                   [(unchecked-divide-int index (row-count maze)) (mod index (col-count maze))]))
+
+               (defn- in-bounds?
+                 "Checks to see if the passed position is in the bounds of the row and columns of the maze.
+                  (in-bounds? 3 3 [4 0])
+                  => false
+                  (in-bounds? 3 3 [1 1])
+                  => true"
+                 [rows cols [x y]]
+                 (not (or (< x 0) (< y 0) (>= x rows) (>= y cols))))
+
+               (defn- valid-move?
+                 "Checks to see if the position is a valid position given a move. Valid positions are only those that are in bounds and
+                  have either a 0 or :E in their spot.
+                  (valid-move? [[:x :x 1] [0 0 1] [1 0 :E]] [1 2])
+                  => false
+                  (valid-move? [[:x :x 1] [0 0 1] [1 0 :E]] [1 1])
+                  => true"
+                 [move pos]
+                 (let [value (get-in move pos)]
+                   (or (= value 0) (= value :E))))
+
+               (defn- next-moves
+                 "Returns only the next valid positions given a maze and a current position.
+                 (next-moves [0 0] [[:x 0 1] [0 0 1] [1 0 :E]])
+                 => ([1 0] [0 1])"
+                 [[x y] maze]
+                 (let [rows      (row-count maze)
+                       cols      (col-count maze)
+                       positions [[(+ x 1) y] [x (+ y 1)] [x (- y 1)] [(- x 1) y]]]
+                   (filter #(and (in-bounds? rows cols %) (valid-move? maze %)) positions)))
+
+               (defn- solved?
+                 "Checks to see if the maze is solved by determining if there is an :E in the maze.
+                  (solved? [[:x :x 1] [1 :x 1] [1 :x :x]])
+                  => true
+                  (solved? [[:x :x 1] [1 :x 1] [1 :x :E]])
+                  => false"
+                 [maze]
+                 (= -1 (.indexOf (flatten maze) :E)))
+
+               (defn- solutions
+                 "Finds all possible solutions to the maze."
+                 [position moves]
+                 (let [current-move (last moves)
+                       next-moves   (next-moves position current-move)]
+                   (if-not (seq next-moves)
+                     (when (solved? current-move)
+                       [moves])
+                     (mapcat #(solutions % (conj moves (assoc-in current-move % :x))) next-moves))))
+
+               (defn solve-maze
+                 "Solves the maze and returns the shortest solution."
+                 [maze]
+                 (let [start-position (start-position maze)
+                       initial-moves  [(assoc-in maze start-position :x)]]
+                   (->> (solutions start-position initial-moves)
+                        (sort-by count)
+                        first
+                        last))))
